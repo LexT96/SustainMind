7bcbbfbfb98934cbfaf8594b102947409ceb165d
burhanyesiloz <burhan.yesiloez@stud.uni-due.de>
2023-06-26 18:05:23 +0200

    rename router, update index

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,15 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { connectToDatabase } from './db/conn.js';
 import { host, port } from './config.js';
-import { userRouter } from './routes/user.router.js';
+import { userRouter } from './routes/userRouter.js';
+import { customerRouter } from './routes/customerrouter.js';
+import { corporationtodoRouter } from './routes/corporationtodorouter.js';
+import { productCategoryRouter } from './routes/productcategoryRouter.js';
+import { productRouter } from './routes/productrouter.js';
+import { productionSiteRouter } from './routes/productionsiteRouter.js';
+import { riskScoreRouter } from './routes/riskscoreRouter.js';
+import { supplierToDoRouter } from './routes/suppliertodoRouter.js';
 import cors from 'cors';
-import { customerRouter } from './routes/customer.router.js';
 
 dotenv.config();
 
@@ -14,8 +20,12 @@ app.use(cors())
 
 app.use('/user', userRouter)
 app.use('/customer', customerRouter)
-
-
+app.use('/product', productRouter)
+app.use('/productcategory', productCategoryRouter)
+app.use('/productionsite', productionSiteRouter)
+app.use('/riskscore', riskScoreRouter)
+app.use('/suppliertodo', supplierToDoRouter)
+app.use('/corporationtodo', corporationtodoRouter)
 
 app.get('/', async (req: Request, res: Response) => {
     res.send("Server is alive and healthy");
