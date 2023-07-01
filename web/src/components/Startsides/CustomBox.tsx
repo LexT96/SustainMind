9f3126a5e344296477bc0d94d48d44c2e78529ce
50367 <ge84pek@mytum.de>
2023-07-01 14:21:53 +0200

    Änderungen der Startseite

---
+++ web/src/components/Startsides/CustomBox.tsx
@@ -0,0 +1,72 @@
+import React, { CSSProperties } from 'react';
+
+interface BoxProps {
+  title: string;
+  description: string;
+}
+
+interface BoxState {
+  isHovered: boolean;
+}
+
+class CustomBox extends React.Component<BoxProps, BoxState> {
+  constructor(props: BoxProps) {
+    super(props);
+    this.state = {
+      isHovered: false,
+    };
+  }
+
+  handleMouseEnter = () => {
+    this.setState({
+      isHovered: true,
+    });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({
+      isHovered: false,
+    });
+  };
+
+  render() {
+    const { title, description } = this.props;
+    const { isHovered } = this.state;
+
+    const boxStyle: CSSProperties = {
+      width: '300px',
+      height: '300px',
+      backgroundColor: 'white',
+      borderRadius: '10px',
+      border: '1px solid black',
+      boxShadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.3)',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: '5px', // Distance between the boxes
+      transition: 'background-color 0.3s', // Smooth transition for background color change
+      cursor: 'pointer', // Cursor change on hover
+    };
+
+    const hoverStyle: CSSProperties = {
+      backgroundColor: 'gray', // Background color change on hover
+    };
+
+    const boxClassName = isHovered ? 'box-hover' : '';
+
+    return (
+      <div
+        style={{ ...boxStyle, ...(isHovered ? hoverStyle : {}) }}
+        className={boxClassName}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <h3>{title}</h3>
+        <p style={{ textAlign: 'center' }}>{description}</p>
+      </div>
+    );
+  }
+}
+
+export default CustomBox;
--- web/src/pages/CorporationMode.tsx
+++ web/src/pages/CorporationMode.tsx
@@ -1,36 +1,49 @@
-import BoxSx from '../components/Startsides/BoxSx';
-import index from '../App.css';
+import React, { CSSProperties } from 'react';
+import CustomBox from '../components/Startsides/CustomBox';
 import { PageLayout } from '../components/PageLayout';
 
-export default function CorporationMode() {
+function CorporationMode() {
   const boxContents = [
-    { title: "My Products", description: "Möglichkeit für eine Beschreibung" },
-    { title: "Suppliers", description: "Möglichkeit für eine Beschreibung" },
+    { title: 'My Products', description: 'Möglichkeit für eine Beschreibung' },
+    { title: 'Suppliers', description: 'Möglichkeit für eine Beschreibung' },
     {
-      title: "Supplier Marketplace",
-      description: "Möglichkeit für eine Beschreibung",
+      title: 'Supplier Marketplace',
+      description: 'Möglichkeit für eine Beschreibung',
     },
     {
-      title: "Risk Analysis",
-      description: "Möglichkeit für eine Beschreibung",
+      title: 'Risk Analysis',
+      description: 'Möglichkeit für eine Beschreibung',
     },
-    { title: "ESG To Do's", description: "Möglichkeit für eine Beschreibung" },
-    { title: "Production Sites", description: "Möglichkeit für eine Beschreibung" },
+    { title: "ESG To Do's", description: 'Möglichkeit für eine Beschreibung' },
+    { title: 'Production Sites', description: 'Möglichkeit für eine Beschreibung' },
   ];
+
+  const gridContainerStyle: CSSProperties = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    maxWidth: '960px',
+    margin: '0 auto',
+    height: '100vh',
+    overflowX: 'hidden',
+    gap: '5px', // Gap between the boxes1
+    marginTop: '100px', // Vertical spacing between the rows
+  };
 
   return (
     <PageLayout>
-      
-   <div className="grid-container">
-      {boxContents.map((content, index) => (
-        <BoxSx
-          key={index}
-          title={content.title}
-          description={content.description}
-        />
-      ))}
-    </div>
+      <div style={gridContainerStyle}>
+        {boxContents.map((content, index) => (
+          <CustomBox
+            key={index}
+            title={content.title}
+            description={content.description}
+          />
+        ))}
+      </div>
     </PageLayout>
-
   );
 }
+
+export default CorporationMode;
