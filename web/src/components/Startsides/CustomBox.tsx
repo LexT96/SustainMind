bda00b19fe9ce7e4b74549dbee4b18611f9abb23
50367 <ge84pek@mytum.de>
2023-07-02 18:32:26 +0200

    Links -Homeside

--- web/src/components/Startsides/CustomBox.tsx
+++ web/src/components/Startsides/CustomBox.tsx
@@ -1,76 +1,68 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface BoxProps {
   title: string;
   description: string;
+  path?: string;
 }
 
 interface BoxState {
   isHovered: boolean;
 }
 
-class CustomBox extends React.Component<BoxProps, BoxState> {
-  constructor(props: BoxProps) {
-    super(props);
-    this.state = {
-      isHovered: false,
-    };
-  }
+const boxStyle: CSSProperties = {
+  width: "300px",
+  height: "300px",
+  backgroundColor: "white",
+  borderRadius: "10px",
+  border: "1px solid black",
+  boxShadow: "0px 2px 4px 1px rgba(0, 0, 0, 0.3)",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "5px", // Distance between the boxes
+  transition: "background-color 0.3s", // Smooth transition for background color change
+  cursor: "pointer", // Cursor change on hover
+};
 
-  handleMouseEnter = () => {
-    this.setState({
-      isHovered: true,
-    });
+const hoverStyle: CSSProperties = {
+  backgroundColor: "gray", // Background color change on hover
+};
+
+const titleStyle: CSSProperties = {
+  fontWeight: "bold", // Make the title bold
+};
+
+const CustomBox = ({ title, description, path}: BoxProps) => {
+  const [isHovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true)
   };
 
-  handleMouseLeave = () => {
-    this.setState({
-      isHovered: false,
-    });
+  const handleMouseLeave = () => {
+    setHovered(false);
   };
 
-  render() {
-    const { title, description } = this.props;
-    const { isHovered } = this.state;
+    const boxClassName = isHovered ? "box-hover" : "";
 
-    const boxStyle: CSSProperties = {
-      width: '300px',
-      height: '300px',
-      backgroundColor: 'white',
-      borderRadius: '10px',
-      border: '1px solid black',
-      boxShadow: '0px 2px 4px 1px rgba(0, 0, 0, 0.3)',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      margin: '5px', // Distance between the boxes
-      transition: 'background-color 0.3s', // Smooth transition for background color change
-      cursor: 'pointer', // Cursor change on hover
-    };
-
-    const hoverStyle: CSSProperties = {
-      backgroundColor: 'gray', // Background color change on hover
-    };
-
-    const boxClassName = isHovered ? 'box-hover' : '';
-
-    const titleStyle: CSSProperties = {
-      fontWeight: 'bold', // Make the title bold
-    };
+    const navigate = useNavigate();
 
     return (
       <div
         style={{ ...boxStyle, ...(isHovered ? hoverStyle : {}) }}
+        onClick={() => navigate(path ?? "")}
         className={boxClassName}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         <h3 style={titleStyle}>{title}</h3>
-        <p style={{ textAlign: 'center' }}>{description}</p>
+        <p style={{ textAlign: "center" }}>{description}</p>
       </div>
     );
-  }
+  
 }
 
 export default CustomBox;
--- web/src/pages/CorporationMode.tsx
+++ web/src/pages/CorporationMode.tsx
@@ -1,34 +1,44 @@
-import React, { CSSProperties } from 'react';
-import CustomBox from '../components/Startsides/CustomBox';
-import { PageLayout } from '../components/PageLayout';
+import React, { CSSProperties } from "react";
+import CustomBox from "../components/Startsides/CustomBox";
+import { PageLayout } from "../components/PageLayout";
 
 function CorporationMode() {
   const boxContents = [
-    { title: 'My Products', description: 'View your products and their ESG scores' },
-    { title: 'Suppliers', description: 'View your suppliers and manage prevention measures' },
     {
-      title: 'Supplier Marketplace',
-      description: 'Find new suppliers who value sustainability',
+      title: "My Products",
+      description: "View your products and their ESG scores",
+      path: "/products",
     },
     {
-      title: 'Risk Analysis',
-      description: 'Analyze risks of all your suppliers',
+      title: "Suppliers",
+      description: "View your suppliers and manage prevention measures", path: "/suppliers",
+    },
+    {
+      title: "Supplier Marketplace",
+      description: "Find new suppliers who value sustainability", path: "/marketplace",
+    },
+    {
+      title: "Risk Analysis",
+      description: "Analyze risks of all your suppliers", path: "/analysis",
     },
     //{ title: "ESG To Do's", description: 'Möglichkeit für eine Beschreibung' },
-    { title: 'Production Sites', description: 'View and add your production sites' },
+    {
+      title: "Production Sites",
+      description: "View and add your production sites", path: "/sites",
+    },
   ];
 
   const gridContainerStyle: CSSProperties = {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    maxWidth: '960px',
-    margin: '0 auto',
-    height: '100vh',
-    overflowX: 'hidden',
-    gap: '5px', // Gap between the boxes1
-    marginTop: '100px', // Vertical spacing between the rows
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    maxWidth: "960px",
+    margin: "0 auto",
+    height: "100vh",
+    overflowX: "hidden",
+    gap: "5px", // Gap between the boxes1
+    marginTop: "100px", // Vertical spacing between the rows
   };
 
   return (
@@ -39,6 +49,7 @@ function CorporationMode() {
             key={index}
             title={content.title}
             description={content.description}
+            path={content.path}
           />
         ))}
       </div>
