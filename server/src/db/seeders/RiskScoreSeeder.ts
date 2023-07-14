903230a52e08386dabc971facd5a7db80b6246d0
Thorsten <tlex1@web.de>
2023-07-14 11:48:49 +0200

    Fix risk score seeder

--- server/src/db/seeders/RiskScoreSeeder.ts
+++ server/src/db/seeders/RiskScoreSeeder.ts
@@ -4,46 +4,6 @@ import { RiskType } from "../../models/riskType.js";
 
 
 const priorities = ["High", "Medium", "Low"];
-
-const riskScores = [
-  {
-    name: "Child Labor",
-    description: "Description of Child Labor",
-  },
-  {
-    name: "Modern Slavery",
-    description: "Description of Modern Slavery",
-  },
-  {
-    name: "No Freedom of Association",
-    description: "Description of No Freedom of Association",
-  },
-  {
-    name: "Poor Labor Rights & Work Safety",
-    description: "Description of Poor Labor Rights and Work Safety",
-  },
-  {
-    name: "Discrimination",
-    description: "Description of Discrimination",
-  },
-  {
-    name: "Waste Water Pollution",
-    description: "Description of Waste Water Pollution",
-  },
-  {
-    name: "Poor Air Quality",
-    description: "Description of Poor Air Quality",
-  },
-  {
-    name: "Release of Heavy Metals",
-    description: "Description of Release of Heavy Metals",
-  },
-  {
-    name: "Inadequate Waste Disposal",
-    description: "Description of Inadequate Waste Disposal",
-  },
-];
-
 export const seedRiskScores = async () => {
     console.log("seeding RiskScores...");
   const productionSites = await ProductionSite.find({});
@@ -51,18 +11,17 @@ export const seedRiskScores = async () => {
   return await Promise.all(
     productionSites.map(async (site) => {
       riskTypes.forEach(async (type) => {
-        const createdRiskScores: any = [];
-        riskScores.forEach(async (score: any) => {
-          score.productionSite = site._id;
-          const newScore = await RiskScore.create({
-            ...score,
-            riskScore: Math.floor(Math.random() * 100),
-            priority: priorities[Math.floor(Math.random() * priorities.length)],
-            riskType: type._id,
-          });
-          createdRiskScores.push(newScore);
+        const newScore = await RiskScore.create({
+          productionSite: site._id,
+          riskType: type._id,
+          priority: priorities[Math.floor(Math.random() * priorities.length)],
+          riskScore: Math.floor(Math.random() * 100),
         });
-        return createdRiskScores;
+        await ProductionSite.findByIdAndUpdate(
+          site._id,
+          { $push: { riskScores: newScore._id } },
+          { new: true }
+        );
       });
 
     })
